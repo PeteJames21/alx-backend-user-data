04f475bd58657eb22d4d9e8e640552899bd1f3de
PeteJames21 <peetjames21@gmail.com>
2024-04-10 23:18:32 +0300

    Check if password matches hash

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     """Hash a password."""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if password mathes the hash."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

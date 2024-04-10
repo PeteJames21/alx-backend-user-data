19fc74e7c537dcfc9d79462461845f9b7645bfc9
PeteJames21 <peetjames21@gmail.com>
2024-04-10 21:43:04 +0300

    Define a custom Formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 Defines a function that obfuscates selected fields in a log message.
 """
 
+import logging
 import re
 from typing import List
 
@@ -17,8 +18,25 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     :param message: a string representing the log line
     :param separator: the string used to separate messages in a log lines
     """
-    fields = '(?:' + '|'.join(fields) + ')'
-    pattern = re.compile(rf'{fields}=(.+?){separator}')
-    for value in pattern.findall(message):
-        message = re.sub(value, redaction, message)
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Redact fields of the record containing personal data."""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)

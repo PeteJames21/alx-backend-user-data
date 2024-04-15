7c663e975a6c4722785dbd708ad2896d03094de2
PeteJames21 <peetjames21@gmail.com>
2024-04-15 22:43:17 +0300

    Check if a path requires authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,8 +11,18 @@ class Auth:
     """Class for handling authentication."""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """TODO: update docstring"""
-        return False
+        """Check if a path requires authentication."""
+        if not path or not excluded_paths:
+            return True
+
+        # Strip trailing slashes
+        excluded_paths = [s.rstrip('/') for s in excluded_paths]
+        path = path.rstrip('/')
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """TODO: implement"""

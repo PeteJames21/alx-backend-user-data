6f0e784025eb38dabc059e9e25d8861c08d610dd
PeteJames21 <peetjames21@gmail.com>
2024-04-18 12:55:45 +0300

    Add method for generating session ids

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,17 @@
 Defines a class for handling session management.
 """
 from .auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Class for handling session authentication."""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Generate a session id for the user_id."""
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

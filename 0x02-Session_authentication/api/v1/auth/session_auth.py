ce221cfbb0cdfb0f5d4c121800e1cd3b75a1469e
PeteJames21 <peetjames21@gmail.com>
2024-04-18 13:01:39 +0300

    Get user id for a given session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Defines a class for handling session management.
 """
 from .auth import Auth
 from uuid import uuid4
+from typing import Union
 
 
 class SessionAuth(Auth):
@@ -17,3 +18,10 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) \
+            -> Union[str, None]:
+        """Get the user id associated with the given session id."""
+        if not session_id or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id, None)

5ae5732a21262384901e542a3046e9ec8951143d
PeteJames21 <peetjames21@gmail.com>
2024-04-18 12:44:48 +0300

    Add class for session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Defines a class for handling session management.
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class for handling session authentication."""
+    pass
